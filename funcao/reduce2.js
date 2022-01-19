0ff3e86787279dbd992d62820d0d09142f9e586f
Ana Beatriz <anabiawalker@gmail.com>
2022-01-19 15:53:56 -0300

    Erro

---
+++ funcao/reduce2.js
@@ -0,0 +1,20 @@
+const alunos = [
+    { nome: "Ana Beatriz", nota: 9.5, bolsista: false },
+    { nome: "Gustavo", nota: 10, bolsista: true },
+    { nome: "Cláudia", nota: 8.7, bolsista: false },
+    { nome: "Júlia", nota: 6.4, bolsista: true },
+    { nome: "Nilson", nota: 8.9, bolsista: false },
+]
+//Desafio 1: Todos os alunos são bolsistas?
+<<<<<<< HEAD
+const todosBolsistas = (resultado, bolsista) => resultado && bolsista 
+=======
+const todosBolsistas = (resultado, bolsista) => resultado && bolsista
+>>>>>>> 507ccb74a06b6623a4393bfbdafece56d16ec3b7
+
+console.log(alunos.map(a => a.bolsista).reduce(todosBolsistas))
+
+//Desafio 2: Algum aluno é bolsista?
+const algumBolsista = (resultado, bolsista) => resultado || bolsista
+
+console.log(alunos.map(a => a.bolsista).reduce(algumBolsista))
